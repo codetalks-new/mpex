82231cda5c950de24dafe0e12e28f62e151b7d2d
banxi1988 <banxi1988@gmail.com>
2018-06-28 18:43:40 +0800

    fix 修正 tsc 打包配置

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -16,6 +16,40 @@ export declare const getSetting: (options?: wx.SettingOptions | undefined) => Pr
 export declare const downloadFile: (options?: wx.DownloadFileOptions | undefined) => Promise<wx.DownloadFileResponse>;
 export declare const saveFile: (options?: wx.SaveFileOptions | undefined) => Promise<wx.SavedFileData>;
 export declare const getSystemInfo: (options?: wx.GetSystemInfoOptions | undefined) => Promise<wx.SystemInfo>;
+export declare const setTabBarItem: (options?: wx.TabBarItem | undefined) => Promise<wx.BaseResponse>;
+/**
+ * 创建一个 WebSocket 连接。使用前请先阅读说明。
+
+基础库 1.7.0 之前，一个微信小程序同时只能有一个 WebSocket 连接，如果当前已存在一个
+WebSocket 连接，会自动关闭该连接，并重新创建一个 WebSocket 连接。基础库版本 1.7.0 及以后，
+支持存在多个 WebSokcet 连接，每次成功调用 wx.connectSocket 会返回一个新的 SocketTask。
+ */
+export declare const connectSocket: (options?: wx.ConnectSocketOptions | undefined) => Promise<wx.BaseResponse>;
+/**
+ * 通过 WebSocket 连接发送数据，需要先 connectSocket，并在 onSocketOpen 回调之后才能发送。
+ */
+export declare const sendSocketMessage: (options?: wx.SendSocketMessageOptions | undefined) => Promise<wx.BaseResponse>;
+/**
+ * 关闭 WebSocket 连接。
+ * //注意这里有时序问题，
+//如果 wx.connectSocket 还没回调 wx.onSocketOpen，而先调用 wx.closeSocket，那么就做不到关闭 WebSocket 的目的。
+//必须在 WebSocket 打开期间调用 wx.closeSocket 才能关闭。
+ */
+export declare const closeSocket: (options?: wx.CloseSocketOptions | undefined) => Promise<wx.BaseResponse>;
+/**
+ * 获取当前的地理位置、速度。当用户离开小程序后，此接口无法调用；当用户点击“显示在聊天顶部”时，此接口可继续调用。
+ */
+export declare const getLocation: (options?: wx.GetLocationOptions | undefined) => Promise<wx.LocationData>;
+/**
+ * 打开地图选择位置。
+ * 需要用户授权 scope.userLocation
+ */
+export declare const chooseLocation: (options?: wx.ChooseLocationOptions | undefined) => Promise<wx.ChooseLocationData>;
+/**
+ * 使用微信内置地图查看位置。
+ * 需要用户授权 scope.userLocation
+ */
+export declare const openLocation: (options?: wx.OpenLocationOptions | undefined) => Promise<wx.BaseResponse>;
 export declare const modalOptions: {
     title: string;
     content: string;
--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * 将普通对象转换成编码成 urlQuery
  *  注意：不支持嵌套
  * @param object 普通对象
  */
-export function encodeQuery(object) {
+function encodeQuery(object) {
     const po = object;
     const kvArray = [];
     for (const key in object) {
@@ -15,18 +17,22 @@ export function encodeQuery(object) {
     }
     return kvArray.join("&");
 }
+exports.encodeQuery = encodeQuery;
 /**
  * 将 微信回调风格的接口函数包装成支持 Promise 风格的函数。
  * @param fun 微信回调风格接口函数
  */
-export function promisify(fun) {
+function promisify(fun) {
     function newFun(options) {
         const opts = options || {};
         return new Promise(function (resolve, reject) {
             opts.success = res => {
                 resolve(res);
             };
-            opts.fail = (reason) => {
+            opts.fail = reason => {
+                if (reason) {
+                    console.error(reason);
+                }
                 reject(reason);
             };
             fun(opts);
@@ -34,14 +40,49 @@ export function promisify(fun) {
     }
     return newFun;
 }
-export const login = promisify(wx.login);
-export const getUserInfo = promisify(wx.getUserInfo);
-export const showModal = promisify(wx.showModal);
-export const getSetting = promisify(wx.getSetting);
-export const downloadFile = promisify(wx.downloadFile);
-export const saveFile = promisify(wx.saveFile);
-export const getSystemInfo = promisify(wx.getSystemInfo);
-export const modalOptions = {
+exports.promisify = promisify;
+exports.login = promisify(wx.login);
+exports.getUserInfo = promisify(wx.getUserInfo);
+exports.showModal = promisify(wx.showModal);
+exports.getSetting = promisify(wx.getSetting);
+exports.downloadFile = promisify(wx.downloadFile);
+exports.saveFile = promisify(wx.saveFile);
+exports.getSystemInfo = promisify(wx.getSystemInfo);
+exports.setTabBarItem = promisify(wx.setTabBarItem);
+/**
+ * 创建一个 WebSocket 连接。使用前请先阅读说明。
+
+基础库 1.7.0 之前，一个微信小程序同时只能有一个 WebSocket 连接，如果当前已存在一个
+WebSocket 连接，会自动关闭该连接，并重新创建一个 WebSocket 连接。基础库版本 1.7.0 及以后，
+支持存在多个 WebSokcet 连接，每次成功调用 wx.connectSocket 会返回一个新的 SocketTask。
+ */
+exports.connectSocket = promisify(wx.connectSocket);
+/**
+ * 通过 WebSocket 连接发送数据，需要先 connectSocket，并在 onSocketOpen 回调之后才能发送。
+ */
+exports.sendSocketMessage = promisify(wx.sendSocketMessage);
+/**
+ * 关闭 WebSocket 连接。
+ * //注意这里有时序问题，
+//如果 wx.connectSocket 还没回调 wx.onSocketOpen，而先调用 wx.closeSocket，那么就做不到关闭 WebSocket 的目的。
+//必须在 WebSocket 打开期间调用 wx.closeSocket 才能关闭。
+ */
+exports.closeSocket = promisify(wx.closeSocket);
+/**
+ * 获取当前的地理位置、速度。当用户离开小程序后，此接口无法调用；当用户点击“显示在聊天顶部”时，此接口可继续调用。
+ */
+exports.getLocation = promisify(wx.getLocation);
+/**
+ * 打开地图选择位置。
+ * 需要用户授权 scope.userLocation
+ */
+exports.chooseLocation = promisify(wx.chooseLocation);
+/**
+ * 使用微信内置地图查看位置。
+ * 需要用户授权 scope.userLocation
+ */
+exports.openLocation = promisify(wx.openLocation);
+exports.modalOptions = {
     title: "提示",
     content: "提示内容",
     showCancel: true,
@@ -50,27 +91,30 @@ export const modalOptions = {
     confirmText: "确定",
     confirmColor: "#3CC51F" //确定按钮的文字颜色,
 };
-export function confirm(content, title = "提示") {
-    const options = Object.assign({}, modalOptions);
+function confirm(content, title = "提示") {
+    const options = Object.assign({}, exports.modalOptions);
     options.title = title;
     options.content = content;
     options.showCancel = false;
-    return showModal(options);
+    return exports.showModal(options);
 }
-export function prompt(content, title = "提示") {
-    const options = Object.assign({}, modalOptions);
+exports.confirm = confirm;
+function prompt(content, title = "提示") {
+    const options = Object.assign({}, exports.modalOptions);
     options.title = title;
     options.content = content;
     options.showCancel = true;
-    return showModal(options);
+    return exports.showModal(options);
 }
-export function showLoading(title = "加载中…") {
+exports.prompt = prompt;
+function showLoading(title = "加载中…") {
     wx.showLoading({
         title: title,
         mask: true //显示透明蒙层，防止触摸穿透,
     });
 }
-export const hideLoading = wx.hideLoading;
+exports.showLoading = showLoading;
+exports.hideLoading = wx.hideLoading;
 const _showToast = promisify(wx.showToast);
 /**
  *
@@ -81,16 +125,19 @@ const _showToast = promisify(wx.showToast);
  * @param duration 提示的延迟时间，单位毫秒，默认：1500
  * @param mask 是否显示透明蒙层，防止触摸穿透，默认：true
  */
-export function showToast(title, icon = "success", image, duration = 1500, mask = true) {
+function showToast(title, icon = "success", image, duration = 1500, mask = true) {
     return _showToast({ title, icon, image, duration, mask });
 }
-export function showSuccess(title) {
+exports.showToast = showToast;
+function showSuccess(title) {
     showToast(title);
 }
-export function showToastLoading(title) {
+exports.showSuccess = showSuccess;
+function showToastLoading(title) {
     showToast(title);
 }
-export function makeNavigationUrl(url, params) {
+exports.showToastLoading = showToastLoading;
+function makeNavigationUrl(url, params) {
     if (url[0] != "/") {
         url = "/" + url;
     }
@@ -101,22 +148,25 @@ export function makeNavigationUrl(url, params) {
         return url;
     }
 }
+exports.makeNavigationUrl = makeNavigationUrl;
 function enhanceNavigation(fun) {
     const pfun = promisify(fun);
     return function navigate(url, params) {
         pfun({ url: makeNavigationUrl(url, params) })
             .then(function (res) {
             console.info("NAV DONE " + url);
-        }).catch(function (reason) {
+        })
+            .catch(function (reason) {
             console.info("NAV FAIL " + url + ", reason:", reason);
         });
     };
 }
-export const navigateTo = enhanceNavigation(wx.navigateTo);
-export const redirectTo = enhanceNavigation(wx.redirectTo);
-export const switchTab = enhanceNavigation(wx.switchTab);
-export const navigateBack = enhanceNavigation(wx.navigateBack);
-export const reLaunch = enhanceNavigation(wx.reLaunch);
-export function setNavigationBarTitle(title) {
+exports.navigateTo = enhanceNavigation(wx.navigateTo);
+exports.redirectTo = enhanceNavigation(wx.redirectTo);
+exports.switchTab = enhanceNavigation(wx.switchTab);
+exports.navigateBack = enhanceNavigation(wx.navigateBack);
+exports.reLaunch = enhanceNavigation(wx.reLaunch);
+function setNavigationBarTitle(title) {
     wx.setNavigationBarTitle({ title });
 }
+exports.setNavigationBarTitle = setNavigationBarTitle;

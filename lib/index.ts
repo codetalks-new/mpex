9eb0a935f2f3b4f85a5b63754ffba06f75b2a088
banxi1988 <banxi1988@gmail.com>
2018-06-28 18:43:25 +0800

    feat add WebSocket 和 Location 模块的接口 promisify 化

--- lib/index.ts
+++ lib/index.ts
@@ -29,14 +29,14 @@ export function promisify<P extends wx.BaseOptions, R>(
       opts.success = res => {
         resolve(res);
       };
-      opts.fail = (reason) => {
-        if(reason){
-          console.error(reason)
+      opts.fail = reason => {
+        if (reason) {
+          console.error(reason);
         }
         reject(reason);
       };
       fun(opts);
-    })
+    });
   }
   return newFun;
 }
@@ -66,6 +66,64 @@ export const saveFile = promisify<wx.SaveFileOptions, wx.SavedFileData>(
 
 export const getSystemInfo = promisify<wx.GetSystemInfoOptions, wx.SystemInfo>(
   wx.getSystemInfo
+);
+
+export const setTabBarItem = promisify<wx.TabBarItem, wx.BaseResponse>(
+  wx.setTabBarItem
+);
+
+/**
+ * 创建一个 WebSocket 连接。使用前请先阅读说明。
+
+基础库 1.7.0 之前，一个微信小程序同时只能有一个 WebSocket 连接，如果当前已存在一个
+WebSocket 连接，会自动关闭该连接，并重新创建一个 WebSocket 连接。基础库版本 1.7.0 及以后，
+支持存在多个 WebSokcet 连接，每次成功调用 wx.connectSocket 会返回一个新的 SocketTask。
+ */
+export const connectSocket = promisify<
+  wx.ConnectSocketOptions,
+  wx.BaseResponse
+>(wx.connectSocket);
+
+/**
+ * 通过 WebSocket 连接发送数据，需要先 connectSocket，并在 onSocketOpen 回调之后才能发送。
+ */
+export const sendSocketMessage = promisify<
+  wx.SendSocketMessageOptions,
+  wx.BaseResponse
+>(wx.sendSocketMessage);
+
+/**
+ * 关闭 WebSocket 连接。
+ * //注意这里有时序问题，
+//如果 wx.connectSocket 还没回调 wx.onSocketOpen，而先调用 wx.closeSocket，那么就做不到关闭 WebSocket 的目的。
+//必须在 WebSocket 打开期间调用 wx.closeSocket 才能关闭。
+ */
+export const closeSocket = promisify<wx.CloseSocketOptions, wx.BaseResponse>(
+  wx.closeSocket
+);
+
+/**
+ * 获取当前的地理位置、速度。当用户离开小程序后，此接口无法调用；当用户点击“显示在聊天顶部”时，此接口可继续调用。
+ */
+export const getLocation = promisify<wx.GetLocationOptions, wx.LocationData>(
+  wx.getLocation
+);
+
+/**
+ * 打开地图选择位置。
+ * 需要用户授权 scope.userLocation
+ */
+export const chooseLocation = promisify<
+  wx.ChooseLocationOptions,
+  wx.ChooseLocationData
+>(wx.chooseLocation);
+
+/**
+ * 使用微信内置地图查看位置。
+ * 需要用户授权 scope.userLocation
+ */
+export const openLocation = promisify<wx.OpenLocationOptions, wx.BaseResponse>(
+  wx.openLocation
 );
 
 export const modalOptions = {
@@ -143,19 +201,19 @@ export function makeNavigationUrl(url: string, params?: object) {
   }
 }
 
-declare var console:any;
+declare var console: any;
 
 type OldNavigationFunction = typeof wx.navigateTo;
 function enhanceNavigation(fun: OldNavigationFunction) {
-  const pfun = promisify(fun)
+  const pfun = promisify(fun);
   return function navigate(url: string, params?: object) {
     pfun({ url: makeNavigationUrl(url, params) })
-    .then(function(res){
-      console.info("NAV DONE "+url)
-    }).catch(function(reason){
-      console.info("NAV FAIL "+url+", reason:",reason)
-    })
-    ;
+      .then(function(res) {
+        console.info("NAV DONE " + url);
+      })
+      .catch(function(reason) {
+        console.info("NAV FAIL " + url + ", reason:", reason);
+      });
   };
 }
 

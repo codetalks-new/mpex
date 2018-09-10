e17391bb34d0964f84572aa1352c19ec3966d1f4
banxi1988 <banxi1988@gmail.com>
2018-09-10 09:50:41 +0800

    fix api 中 options 参数设置为可选的

--- lib/utils.ts
+++ lib/utils.ts
@@ -21,10 +21,10 @@ export function encodeQuery(object: object): string {
  * @param fun 微信回调风格接口函数
  */
 export function promisify<P extends wx.BaseOptions, R>(
-  fun: (options: P) => any,
+  fun: (options?: P) => any,
   thisInstance?: object
 ) {
-  const newFun = function(options: P) {
+  const newFun = function(options?: P) {
     const opts = options || ({} as any);
     return new Promise<R>(function(resolve, reject) {
       opts.success = (res: any) => {
